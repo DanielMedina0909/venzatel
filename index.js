2d735808cb8cc691309a31238a905b0e308339b7
DanielMedina0909 <155011383+DanielMedina0909@users.noreply.github.com>
2024-06-29 13:34:27 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const botonEnConstruccion = document.querySelector('.en-construccion');
+
+botonEnConstruccion.addEventListener('click', () => {
+  alert('¡Gracias por tu interés! Te notificaremos cuando la página esté lista.');
+});
